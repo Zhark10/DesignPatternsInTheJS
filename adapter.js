8acdcbefdbd5f06606e4c9a92d638b96467facd5
Zhark10 <arkady.zharavin@citronium.biz>
2020-01-17 00:23:14 +0300

    :hammer: Adapter Design Pattern

---
+++ adapter.js
@@ -0,0 +1,46 @@
+class BadSupportService {
+    help(message, type = 'hardly') {
+        if (message.length > 10 || type === 'hardly') {
+            console.log('I can`t help you!')
+        } else {
+            console.log('I try to help you')
+        }
+    }
+}
+
+class GoodSupportService {
+    helpByHardQuestion(message) {
+        const answer = 'I will help you'
+        console.log(`Question: ${message}. Answer: ${answer}`)
+    }
+
+    helpBySimpleQuestion(message) {
+        const answer = 'You may think that I have already helped you'
+        console.log(`Question: ${message}. Answer: ${answer}`)
+    }
+}
+
+class AdapterForNewSupport {
+    static initialized 
+    constructor() {
+        this.initialized = new GoodSupportService()
+    }
+
+    help(message, type = 'hardly') {
+        if (message.length > 10 || type === 'hardly') {
+            this.initialized.helpByHardQuestion(message)
+        } else {
+            this.initialized.helpBySimpleQuestion(message)
+        }
+    }
+}
+
+const badSupport = new BadSupportService();
+console.log(badSupport.help('please, help me!', 'simple'))
+
+const goodSupport = new GoodSupportService();
+console.log(goodSupport.helpByHardQuestion('please, help me!'))
+console.log(goodSupport.helpBySimpleQuestion('please, help me!'))
+
+const adaptedSupport = new AdapterForNewSupport();
+console.log(adaptedSupport.help('please, help me!', 'simple'))
